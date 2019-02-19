11d368c97e8fdf40539f1543cf986f61e7cfa0bc
annika <annikaslund@gmail.com>
2019-02-18 17:14:55 -0800

    Complete practice problems up to 15

--- queries.sql
+++ queries.sql
@@ -33,3 +33,61 @@ SELECT category, AVG(rating) AS average_rating
 	FROM analytics
 	GROUP BY category 
 	ORDER BY average_rating DESC;
+
+--Most expensive app with a rating that is less than 3 --
+SELECT app_name, price, rating
+	FROM analytics
+	WHERE rating < 3
+	ORDER BY price desc
+	LIMIT 1;
+
+--all apps with a min install not exveeding 50  and has a rating --
+SELECT app_name, min_installs,rating 
+	FROM analytics
+	WHERE min_installs < 50 AND rating is not NULL
+	ORDER BY rating DESC;
+
+-- all apps that are rated less than three with at least 10000 reviews--
+SELECT app_name, rating, reviews
+	FROM analytics
+		WHERE rating < 3 AND reviews >= 10000;
+
+--Top ten most revied apps that cost between 10 cents and 99 cents --
+SELECT app_name, reviews
+	FROM analytics
+		WHERE price BETWEEN .10 AND .999999
+		ORDER BY reviews DESC
+		LIMIT 10;
+
+-- most outdated app --
+SELECT *
+	FROM analytics
+	WHERE last_updated = (Select MIN(last_updated) FROM analytics);        
+
+-- most expensive app --
+SELECT *
+	FROM analytics
+	WHERE price = (Select MAX(price) FROM analytics);
+
+
+-- sum of all reviews -- 
+SELECT SUM(reviews) as review_sum
+	FROM analytics;
+
+-- categories that have more than 300 apps --
+SELECT category, COUNT(category)
+	FROM analytics
+		GROUP BY category
+			HAVING COUNT(category) > 300;
+
+-- app that has highest proportion of min_installs to reviews, among apps that have been installed at least 100,000 times--
+SELECT app_name, reviews, min_installs, (min_installs/reviews) AS proportion
+	FROM analytics	
+	WHERE min_installs > 100000
+	ORDER BY proportion DESC
+	LIMIT 1;
+
+
+-- --
+
+	
